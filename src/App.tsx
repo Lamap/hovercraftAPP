b4622b2d63a3ea27db78a0c9ba785c0d24303039
lenkei.akos <lenkei.akos@virgo.hu>
2018-03-13 17:24:41 +0100

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import './App.css';
 import Bela from './components/bela';
 
-const logo = require('./logo.svg');
+const logo = require('./minion.svg');
 
 class App extends React.Component {
   render() {
@@ -10,12 +10,9 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to hovercraft</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-          <Bela name="jenő" />
+        <Bela name="jenő" />
       </div>
     );
   }
