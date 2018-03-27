0ec6b6ffb68fe772ad4cb82c76a64b9d006b6be3
lenkei.akos <lenkei.akos@virgo.hu>
2018-03-27 17:54:39 +0200

    masonry layout

--- src/components/imageItem/imageItem.tsx
+++ src/components/imageItem/imageItem.tsx
@@ -1,35 +1,52 @@
 import * as React from 'react';
+import './imageItem.css';
 import { Typeahead } from 'react-bootstrap-typeahead';
 
 export interface Props {
     image: any;
     tags: any[];
 }
+export interface State {
+    showStaticTags: boolean;
+    showDynamicTags: boolean;
+}
+class ImageItem extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            showStaticTags: false,
+            showDynamicTags: true
+        };
+    }
 
-class ImageItem extends React.Component<Props, object> {
     changed(items: string[]) {
         console.log('changed', items);
+        // TODO: do the comparison with the previous array, send new tag to the API
     }
     render() {
         return (
-            <div>
+            <div className="image-item">
                 <img
-                    className="image-item"
-                    src={this.props.image.src + '&sz=w250'}
+                    className="image-item__image"
+                    src={this.props.image.src + '&sz=w240'}
                 />
-                <div className="image-item__tags">
-                    {this.props.image.tags.map((tag: any) =>
-                        <span className="image-item__tag label label-info" key={tag}>{tag}</span>)}
-                </div>
+                {this.state.showStaticTags ?
+                    <div className="image-item__tags">
+                        {
+                            this.props.image.tags.map((tag: any) =>
+                            <span className="image-item__tag label label-info" key={tag}>{tag}</span>
+                        )}
+                    </div> : ''
+                }
                 <div className="image-item__typeahead">
                     <Typeahead
                         options={this.props.tags}
                         multiple={true}
                         align="left"
                         allowNew={true}
-                        submitFormOnEnter={true}
                         onChange={this.changed}
                         defaultSelected={this.props.image.tags}
+                        placeholder="Add tag"
                     />
                 </div>
             </div>
--- src/components/imageList/imageList.tsx
+++ src/components/imageList/imageList.tsx
@@ -8,18 +8,44 @@ export interface Props {
     images: any[];
     tags: any[];
 }
+export interface State {
+    colCount: number;
+}
 
-class ImageList extends React.Component<Props, object> {
+class ImageList extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            colCount: 5
+        };
+    }
+    getCols() {
+        let cols: any[] = [];
+
+        for (let colIndex = 0; colIndex < this.state.colCount; colIndex++) {
+            cols[colIndex] = this.props.images.filter((image, imageIndex) => {
+                return imageIndex % this.state.colCount === colIndex;
+            });
+        }
+
+        return cols;
+    }
     render() {
+        const cols = this.getCols();
         return (
-            <div className="imagelist">
-                    {this.props.images.map(image =>
-                    <div
-                        key={image.src}
-                        className="imagelist__item"
-                    >
-                        <ImageItem image={image} tags={this.props.tags}/>
-                    </div>)}
+            <div className={'imagelist imagelist--divided-' + this.state.colCount}>
+                {
+                    cols.map((col, colIndex) =>
+                        <div key={colIndex} className="imagelist__col">
+                            {col.map(
+                                (image: any, imageIndex: number) => <ImageItem
+                                    key={imageIndex}
+                                    image={image}
+                                    tags={this.props.tags}
+                                />
+                            )}
+                        </div>)
+                }
             </div>
         );
     }
